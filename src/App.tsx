9cf3433276bc46ccbd498124632a65cc7554ca6f
ASIM PAUDEL <asimpaudel48@gmail.com>
2025-09-16 12:05:02 +0545

    Remove Login and Register routes from App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LanguageTrack from './pages/LanguageTrack'
 import LevelPage from './pages/LevelPage'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50">
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="/:language" element={<LanguageTrack />} />
           <Route path="/:language/level/:levelId" element={<LevelPage />} />
@@ -22,3 +18,4 @@ function App() {
 }
 
 export default App
+
