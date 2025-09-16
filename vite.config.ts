4f2c60b2f0bcd5982ea1e738fab4b8918532c3b9
ASIM PAUDEL <asimpaudel48@gmail.com>
2025-09-16 10:57:47 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/programming-academy/' : '',
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true
   }
 }))
+
