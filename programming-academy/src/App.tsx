aafd6bc35d14a53780cf25338ee3bc6d2ed11ce1
ASIM PAUDEL <asimpaudel48@gmail.com>
2025-09-16 11:42:51 +0545

    Refactor App component by removing unnecessary providers

--- programming-academy/src/App.tsx
+++ programming-academy/src/App.tsx
@@ -1,4 +1,3 @@
-/* React import not needed in >=17 */
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LanguageTrack from './pages/LanguageTrack'
@@ -8,24 +7,18 @@ import RegisterPage from './pages/RegisterPage'
 
 function App() {
   return (
-    <AuthProvider>
-      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-        <ProgressProvider>
-          <Router>
-            <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50">
-              <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/" element={<HomePage />} />
-                <Route path="/:language" element={<LanguageTrack />} />
-                <Route path="/:language/level/:levelId" element={<LevelPage />} />
-              </Routes>
-            </div>
-          </Router>
-        </ProgressProvider>
-      </GoogleOAuthProvider>
-    </AuthProvider>
+    <Router>
+      <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:language" element={<LanguageTrack />} />
+          <Route path="/:language/level/:levelId" element={<LevelPage />} />
+        </Routes>
+      </div>
+    </Router>
   )
- }
- 
- export default App
+}
+
+export default App
